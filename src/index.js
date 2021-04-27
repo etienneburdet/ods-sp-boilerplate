d92c481a4e3a11c578a218cf066fbf22137e98c9
Frédéric Passaniti <f.passaniti@gmail.com>
2021-04-27 15:40:44 +0200

    Mise à jour de la lib de widget fix du css manquant ajout d'un exemple de requête avec le sdk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { ApiClient, fromCatalog } from '@opendatasoft/api-client';
 
-import confetti from 'canvas-confetti';
+// Initialize the Client by indicating the domain to request.
+const client = new ApiClient({ domain: "documentation-resources" });
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// Create the query to run.
+const query = fromCatalog() // From the domain catalog
+    .dataset("doc-geonames-cities-5000") // ... we'll use the dataset "doc-geonames-cities-5000"
+    .aggregates() // ... in order to make an aggregation.
+    .where("country_code:'FR'") // // Filter records where country_code === "FR".
+    .groupBy("name as city, population") // Select the fields "name" and "population".
+    .orderBy("-population") // Sort by population in descending order.
+    .limit(10) // But we only want the first 10 most populated cities.
+    .toString(); // Then finally, we convert our query into a string.
+
+// Now, run the query.
+client.get(query)
+    .then(response => {
+        var ul = document.createElement('ul');
+        response.aggregations.forEach((aggreg) => {
+            var li = document.createElement('li');
+            li.innerText = aggreg.city + ' (' + aggreg.population + ' habitants)';
+            ul.appendChild(li);
+        });
+        document.getElementById('results').innerHTML = ul.outerHTML;
+        console.log(response);
+    })
+    .catch(error => console.error(error));
